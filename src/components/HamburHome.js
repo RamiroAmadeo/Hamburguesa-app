6b80f1815fc057d35b3d31fadd8f85b913bd4352
Ramiro <raamiroamadeo@gmail.com>
2023-06-02 18:32:24 -0300

    keep creating the home page

--- src/components/HamburHome.js
+++ src/components/HamburHome.js
@@ -1,8 +1,33 @@
 import hamburHomeStyle from './HamburHome.module.css'
+import image1 from '../assets/mostrando-hamburguesa.jpg'
+import image2 from '../assets/dos-personas-comiendo.jpg'
+import image3 from '../assets/una-persona-comiendo.jpg'
 
 const HamburHome = () => {
     return (
-        <h1 className={hamburHomeStyle.container}>SARPADAS EN RICAS</h1>
+        <>
+            <h1 className={hamburHomeStyle.container}>SARPADAS EN RICAS</h1>
+            <section className={hamburHomeStyle.section}>
+                <div className={hamburHomeStyle.box1}>
+                    <h6>Rosario</h6>
+                    <h3>Nombre local</h3>
+                    <p>Burguers, picadas y mucha onda. Te esperamos.</p>
+                </div>
+                <div className={hamburHomeStyle.boxImage}>
+                    <img className={hamburHomeStyle.image} src={image1} alt=''></img>
+                </div>
+                <div className={hamburHomeStyle.boxImage}>
+                    <img className={hamburHomeStyle.image} src={image2} alt=''></img>
+                </div>
+                <div className={hamburHomeStyle.boxImage}>
+                    <img className={hamburHomeStyle.image} src={image3} alt=''></img>
+                </div>
+            </section>
+            <div className={hamburHomeStyle.menuBox}>
+                <h3 className={hamburHomeStyle.menu}>NUESTRO MENU</h3>
+                <button>VER</button>
+            </div>
+        </>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import HamburHome from "../components/HamburHome";
 import Navbar from "../components/Navbar";
-import fondo from "../assets/fondo-hamburguesas.jpg"
+/* import fondo from "../assets/fondo-hamburguesas.jpg" */
 
 const Home = () => {
     return(
